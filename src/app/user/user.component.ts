9273d2b70a7bae0acc789b298be1696ab6c3d0e0
Your Name <you@example.com>
2023-12-31 01:26:03 -0500

    admin

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {CdkMenuTrigger} from "@angular/cdk/menu";
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [
+    MatSlideToggleModule,
+    CdkMenuTrigger
+  ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
