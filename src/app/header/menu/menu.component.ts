1b56fcbaa4ff52ba14d443ae442b8f9b9039069a
Your Name <you@example.com>
2023-12-31 21:06:47 -0500

    menu

--- src/app/header/menu/menu.component.ts
+++ src/app/header/menu/menu.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
+import {MatMenuModule} from "@angular/material/menu";
 
 @Component({
   selector: 'app-menu',
@@ -9,7 +10,8 @@ import {MatButtonModule} from "@angular/material/button";
   imports: [
     MatToolbarModule,
     MatIconModule,
-    MatButtonModule
+    MatButtonModule,
+    MatMenuModule
   ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
