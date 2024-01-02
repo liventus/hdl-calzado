4e5615595fc9f420951647ac1d389c685fdf4df2
Your Name <you@example.com>
2024-01-02 00:30:06 -0500

    angular

--- src/app/body/inicio-component/inicio-component.component.ts
+++ src/app/body/inicio-component/inicio-component.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-
+import {MatCardModule} from '@angular/material/card';
 
 
 @Component({
   selector: 'app-inicio-component',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardModule
+  ],
   templateUrl: './inicio-component.component.html',
   styleUrl: './inicio-component.component.css',
 
