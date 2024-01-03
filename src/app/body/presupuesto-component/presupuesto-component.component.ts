f7e026267548cf47a050aa879cf92f30de053bd6
Your Name <you@example.com>
2024-01-03 18:44:54 -0500

    se agrega div con flexbox

--- src/app/body/presupuesto-component/presupuesto-component.component.ts
+++ src/app/body/presupuesto-component/presupuesto-component.component.ts
@@ -6,6 +6,8 @@ import {BreakpointObserver,Breakpoints} from "@angular/cdk/layout";
 import { distinctUntilChanged, tap } from 'rxjs/operators';
 import {MatCardModule} from "@angular/material/card";
 import {NgSwitch, NgSwitchCase} from "@angular/common";
+import {MatButtonModule} from "@angular/material/button";
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 
 @Component({
@@ -15,7 +17,9 @@ import {NgSwitch, NgSwitchCase} from "@angular/common";
     MatGridListModule,
     MatCardModule,
     NgSwitch,
-    NgSwitchCase
+    NgSwitchCase,
+    MatButtonModule,
+    MatToolbarModule
   ],
   templateUrl: './presupuesto-component.component.html',
   styleUrl: './presupuesto-component.component.css',
@@ -23,42 +27,19 @@ import {NgSwitch, NgSwitchCase} from "@angular/common";
 })
 export default class PresupuestoComponentComponent implements OnInit{
 
-  Breakpoints = Breakpoints;
-  currentBreakpoint:string = '';
 
-  readonly breakpoint$ = this.breakpointObserver
-    .observe([Breakpoints.Large, Breakpoints.Medium, Breakpoints.Small, '(min-width: 500px)'])
-    .pipe(
-      tap(value => console.log(value)),
-      distinctUntilChanged()
-    );
+
+
 
   constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
-    this.breakpoint$.subscribe(() =>
-      this.breakpointChanged()
-    );
+
   }
 
 
-  private breakpointChanged() {
-    if(this.breakpointObserver.isMatched(Breakpoints.Large)) {
-      this.currentBreakpoint = Breakpoints.Large;
-    } else if(this.breakpointObserver.isMatched(Breakpoints.Medium)) {
-      this.currentBreakpoint = Breakpoints.Medium;
-    } else if(this.breakpointObserver.isMatched(Breakpoints.Small)) {
-      this.currentBreakpoint = Breakpoints.Small;
-    } else if(this.breakpointObserver.isMatched('(min-width: 500px)')) {
-      this.currentBreakpoint = '(min-width: 500px)';
-    }
-  }
+
 
 }
 
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
+
